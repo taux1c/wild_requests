1383d6e6aa5426a6985397ecc86de2545f8e2fcd
Taux1c <24677318+taux1c@users.noreply.github.com>
2023-11-01 16:25:24 -0400

    Update setup.py
    Update info.

--- setup.py
+++ setup.py
@@ -3,10 +3,8 @@ from setuptools import setup
 setup(
     name='wild_requests',
     version='Beta 1.0.0',
-    packages=[''],
-    url='',
-    license='',
+    packages=find_packages(),
+    url='https://github.com/taux1c/wild_requests',
     author='Taux1c',
-    author_email='',
     description='Just a module that provides some tools for downloading.'
 )
