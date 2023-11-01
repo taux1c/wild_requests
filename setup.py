569788ef01ef616c502ee148adfaa3db748d393c
Taux1c <24677318+taux1c@users.noreply.github.com>
2023-11-01 16:41:27 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='wild_requests',
     version='0.0.1',
-    py_modules=['wild_requests'],  # Use 'py_modules' for single modules.
+    py_modules=['wild_requests'], 
     url='https://github.com/taux1c/wild_requests',
     author='Taux1c',
     description='Just a module that provides some tools for downloading.'
