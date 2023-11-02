684387c0999a01695f3eee48220bf69be968cfa6
Taux1c <24677318+taux1c@users.noreply.github.com>
2023-11-02 00:32:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     author='Taux1c',
     install_requires=['httpx~=0.25.0'],
     description='Just a module that provides some tools for downloading.',
-    long_description="""Wild Requests is a versatile Python package designed to simplify HTTP requests using the powerful HTTPX library. With an intuitive interface and prebuilt logic, it streamlines the process of making web requests, making it an ideal choice for both beginners and experienced developers. Wild Requests supports various HTTP methods, handles authentication, and manages cookies effortlessly. Whether you're building web scrapers, API clients, or need to access web resources, Wild Requests offers a convenient and efficient solution."""
+    long_description="""Wild Requests is a versatile Python package designed to simplify HTTP requests using the powerful HTTPX library. With an intuitive interface and prebuilt logic, it streamlines the process of making web requests, making it an ideal choice for both beginners and experienced developers. Wild Requests supports various HTTP methods, handles authentication, and manages cookies effortlessly. Whether you're building web scrapers, API clients, or need to access web resources, Wild Requests offers a convenient and efficient solution.""",
     long_description_content_type='text/x-rst'
 )
